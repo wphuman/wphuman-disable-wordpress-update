09176324af37e55a5bc6b86002c7f36697081a35
Tang Rufus <TangRufus@users.noreply.github.com>
2015-01-06 22:36:49 +0800

    Disable WPMUDEV update notice

--- wphuman-disable-wordpress-update.php
+++ wphuman-disable-wordpress-update.php
@@ -2,12 +2,12 @@
 
 /**
  * Plugin Name:         WP Human Disable WordPress Update
- * Description:         Disable WordPress Update.  Made by Tang Rufus from WP Human
+ * Description:         Disable core WordPress & WPMUDEV update notice.  Made by Tang Rufus from WP Human
  * Author:              Tang Rufus @ WP Human
  * Author URI:          http://wphuman.com
  * Author Twitter:      @tangrufus, @wphuman
  * Author Email:        rufus@wphuman.com
- * Version:             1.1.2
+ * Version:             1.2.0
  * License:             GPL-2.0+
  * License URI:         http://www.gnu.org/licenses/gpl-2.0.txt
  * GitHub Plugin URI: 	https://github.com/wphuman/wphuman-disable-wordpress-update
@@ -15,7 +15,7 @@
  *
  */
 
-/** Disable the Update Notice */
+/** Disable the core update notice */
 add_filter( 'pre_site_transient_update_core',  '__return_null' ); // screening core update
 add_filter( 'pre_site_transient_update_plugins',  '__return_null' ); // screening plugin update
 add_filter( 'pre_site_transient_update_themes',  '__return_null' ); // screening theme update
@@ -27,3 +27,7 @@ remove_action( 'admin_init', '_maybe_update_themes'); // forbid wp_update_themes
 wp_clear_scheduled_hook( 'wp_update_themes' );
 wp_clear_scheduled_hook( 'wp_update_plugins' );
 wp_clear_scheduled_hook( 'wp_version_check' );
+
+/** Disable WPMUDEV update notice */
+remove_action( 'admin_notices', 'wdp_un_check', 5 );
+remove_action( 'network_admin_notices', 'wdp_un_check', 5 );
